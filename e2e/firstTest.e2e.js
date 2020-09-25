982dbe8d30f6e21a269eb1911b8f5d3c514193e7
Oktay Ibis <oktayibis@gmail.com>
2020-09-25 08:43:04 +0300

    init detox

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -3,6 +3,7 @@ describe('App', () => {
     await device.reloadReactNative();
   });
 
+  // Initial test case
   it('Render All Page', async () => {
     await expect(element(by.id('testOne'))).toBeVisible();
   });
